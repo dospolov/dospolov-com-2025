835e4f25c439341d464e5586ec1c172561f38e5c
Marat Dospolov <marat.dospolov@gmail.com>
2025-02-26 23:59:11 +0100

    home link

--- site.config.ts
+++ site.config.ts
@@ -50,16 +50,14 @@ export default siteConfig({
 
   // whether to use the default notion navigation style or a custom one with links to
   // important pages. To use `navigationLinks`, set `navigationStyle` to `custom`.
-  navigationStyle: 'default'
-  // navigationStyle: 'custom',
-  // navigationLinks: [
-  //   {
-  //     title: 'About',
-  //     pageId: 'f1199d37579b41cbabfc0b5174f4256a'
-  //   },
-  //   {
-  //     title: 'Contact',
-  //     pageId: '6a29ebcb935a4f0689fe661ab5f3b8d1'
-  //   }
-  // ]
+  // navigationStyle: 'default'
+  navigationStyle: 'custom',
+  navigationLinks: [
+    {
+      title: 'Home',
+      pageId: '1a6b54089ce480f08219d2a882d3e9d6'
+    }
+  ],
+
+  isSearchEnabled: false
 })
